93a9d311598ed18240775d04b200f623ff2ce64f
raj dhake <your.raj12345dhake@gmail.com>
2020-06-17 21:47:57 +0530

    after the user sign-in, the user will to redirected to homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import ShopPage from './pages/shop/ShopPage';
  import Header from './components/Header';
 import SignInSignUpPage from './pages/sign-in-sign-up/SignInSignUpPage';
@@ -35,7 +35,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInSignUpPage}/>
+        <Route exact path='/signin' render={()=> currentUser? (<Redirect to='/'/>): (<SignInSignUpPage/>)}/>
         
       </Switch>
     </div>
