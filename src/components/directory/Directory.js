fdf91d1374d5322a516f3e59d6d154fab172b90c
raj dhake <65240809+rajdhake@users.noreply.github.com>
2020-07-01 13:19:54 +0530

    updated the directory component

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -8,33 +8,33 @@ function Directory() {
           title: 'hats',
           imageUrl: 'https://images.pexels.com/photos/35185/hats-fedora-hat-manufacture-stack.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'shop'
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
-          linkUrl: 'shop/jackets'
+          linkUrl: 'shop'
         },
         {
           title: 'sneakers',
           imageUrl: 'https://images.pexels.com/photos/1032110/pexels-photo-1032110.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
           id: 3,
-          linkUrl: 'shop/sneakers'
+          linkUrl: 'shop'
         },
         {
           title: 'womens',
           imageUrl: 'https://images.pexels.com/photos/908602/pexels-photo-908602.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
           size: 'large',
           id: 4,
-          linkUrl: 'shop/womens'
+          linkUrl: 'shop'
         },
         {
           title: 'mens',
           imageUrl: 'https://images.pexels.com/photos/1342609/pexels-photo-1342609.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
           size: 'large',
           id: 5,
-          linkUrl: 'shop/mens'
+          linkUrl: 'shop'
         }
       ])
       
